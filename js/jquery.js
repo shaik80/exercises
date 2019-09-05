b79e198e3ccab6e9875d16e648c42a2089409664
shaik80 <sh.mudassir98@gmail.com>
2019-09-05 17:41:30 +0530

    css

---
+++ js/jquery.js
@@ -0,0 +1,11 @@
+// Traversal & Manipulation
+// Event handlers
+// AJAX (External Requests)
+// document.getElementsByClassName("weather-app")
+// Id
+let weatherApp = $('#weather-app')
+// console.log(weatherApp.text())
+let cityWeather = $('#weather-app>.city-weather')
+console.log(cityWeather.html())
+// cityWeather.html('Chennai');
+// console.log(cityWeather.html())
